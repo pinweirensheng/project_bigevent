05164d17bb6bf70e086af369a3cebcd464e57400
zyh <zyh0623@163.com>
2021-06-01 11:45:12 +0800

    注册 页面书写完毕

---
+++ assets/js/login.js
@@ -0,0 +1,40 @@
+$(function () {  
+    $(".login-box").on('click',function () {  
+        $('.login-content').hide();
+        $(".reg-content").show();
+    })
+    $(".reg-box").on('click',function () {  
+        $('.login-content').show();
+        $(".reg-content").hide();
+    })
+    var form = layui.form;
+    form.verify({ 
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+        ],
+        repwd:function (value) {  
+            var pwd = $("#form-reg [name=password]").val();
+            if(pwd !== value){
+                return '两次输入的密码不同';
+            }
+        }
+    });  
+    //注册ajax的获取数据
+    $('#form-reg').on('submit',function (e) {  
+        e.preventDefault();
+        // console.log($('#form-reg [name=password]').val())
+        var data = {
+            username:$("#form-reg [name=username]").val(),
+            password:$('#form-reg [name=password]').val()
+        }
+        $.post('http://api-breakingnews-web.itheima.net/api/reguser',data,function (res) { 
+            console.log(res)
+            if(res.status===0){
+                $(".reg-box").click();
+                $("#form_login [name=username]").val($("#form-reg [name=username]").val())
+                $("#form_login [name=password]").val($("#form-reg [name=password]").val())
+            }
+        })
+    })
+})
